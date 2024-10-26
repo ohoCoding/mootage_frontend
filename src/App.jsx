39414e41aa427343febccf57a1445639bbb48301
ohoCoding <youngho7482@gmail.com>
2024-10-26 20:26:42 +0900

    App.jsx edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
+import { Outlet, useParams } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import { useContext, useEffect } from 'react';
+import { ProductContext } from './context/ProductContect';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {filterProducts} = useContext(ProductContext);
+  const {category} = useParams();
+  useEffect(()=>{filterProducts(category)},[category])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen h-auto bg-slate-200 pb-40'>
+      <Navbar/>
+        <div className='w-[70%] m-auto p-10 bg-white my-4'>
+      <Outlet/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    </div>
+  );
 }
 
-export default App
+export default App;
